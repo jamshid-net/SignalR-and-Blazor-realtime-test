fa192bd295bc4b5cb4a66289ac812beea6cf81e4
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-19 22:54:44 +0500

    Add project files.

---
+++ RealtimeDataApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealtimeDataApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        string _connectionString = "Server=JAMSHID-PC\\SQLEXPRESS;Database=CompanyDatabase;Trusted_Connection=True;Encrypt=False;";
+
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
